4fae178d86a1cba4b9212f5cc812aa9c10342764
Mahmoud Siam <122642587+Al-siam@users.noreply.github.com>
2023-05-18 13:09:50 +0300

    Add files via upload

---
+++ from.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+    
+  <h2 class="animated-heading">Optimism makes the impossible possible</h2> 
+   <h2 class="animated-heading"> Mahmoud Siam is Worck </h2>
+
+
+    <form action="from.php"method="POST">
+
+
+<div class="form">
+
+
+<h1>Welcom to my site</h1>
+<div class="inp">  <input type="submit" value="New usre"></div>
+
+
+
+  <div class="inp">  <input type="submit" value="Have an account">
+ </div>
+
+
+
+
+</div>
+
+
+
+
+
+    </form>
+
+
+
+
+
+</body>
+</html>
